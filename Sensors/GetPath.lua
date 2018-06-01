9dddecf39f401205e7c57d1f8073a9146d241421
Petr Houška <houskape@gmail.com>
2018-06-02 01:20:37 +0200

    GetPath now prefers paths that don't go high-er

--- Sensors/GetPath.lua
+++ Sensors/GetPath.lua
@@ -120,21 +120,34 @@ return function(startPos, destPos, boolGridObj)
     local destYi = math.floor(destPos.z / boolGridObj.granularity) + 1
     local dest = {x=destXi, y=destYi}
 
-    local bfsQ = newQueue()
-    
-    local graphGrid = boolGridObj.grid   
-    local navGraph = {}
-    
-    local function neighborExistsSafeNotExplored(nb, navGraph, graphGrid)
+
+    -- The whole current distance and hight keeping in navGraph is an ugly hack to add
+    -- ..some non-one distances support to BFS. I.e to add support for prefering certain
+    -- ..(lower hight) neighbor edges to others. A dijstra / a* with a proper distance 
+    -- ..that captures the preference of not going higher should be used instead. 
+    -- ..In case of refactoring: remember this heap: https://github.com/geoffleyland/lua-heaps
+
+    local function neighborExistsSafeNotExploredOrBetter(nb, currLoc, navGraph, graphGrid)
         if not (nb.x >= 1 and nb.x <= #graphGrid) then return false end         -- neighbor not on map in x -> false  
         if not (nb.y >= 1 and nb.y <= #(graphGrid[1])) then return false end    -- neighbor not on map in y -> false  
-
-        return graphGrid[nb.x][nb.y].safe and navGraph[nb.x][nb.y] == nil       -- neighbor safe && not-yet visited
+        
+        if not (graphGrid[nb.x][nb.y].safe) then return false end -- nb not safe -> false
+        if navGraph[nb.x][nb.y] == nil then return true end       -- nb not visited -> true
+        
+        if navGraph[nb.x][nb.y].dis <= navGraph[currLoc.x][currLoc.y].dis then return false end -- if nb isn't from this front -> false  
+        return graphGrid[currLoc.x][currLoc.y].loc.y < navGraph[nb.x][nb.y].predHig -- nb's prede is of same distance as curr and curr is better 
     end
-
-    local nbs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {1, 1}, {-1, 1}, {1, -1}}
-
     
+    local nbs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {1, 1}, {-1, 1}, {1, -1}}
+    
+    local bfsQ = newQueue()
+    local graphGrid = boolGridObj.grid   
+
+    local navGraph = {}
+    navGraph[dest.x] = {}
+    navGraph[dest.x][dest.y] = {pred = dest, predHig=graphGrid[dest.x][dest.y].loc.y, dis = 0}
+
+    -- create predecessor based navGraph trough modified BFS
     bfsQ:push_right(dest)
     while not bfsQ:is_empty() do
         local currLoc = bfsQ:pop_right()
@@ -143,19 +156,26 @@ return function(startPos, destPos, boolGridObj)
             if navGraph[nb.x] == nil then navGraph[nb.x] = {} end   
 
             -- if the neighbor is safe & hasn't been explored yet -> add to navGraph & queue
-            if neighborExistsSafeNotExplored(nb, navGraph, graphGrid) then
-                navGraph[nb.x][nb.y] = currLoc
-                bfsQ:push_left(nb)                                
+            if neighborExistsSafeNotExploredOrBetter(nb, currLoc, navGraph, graphGrid) then
+                if navGraph[nb.x][nb.y] == nil then bfsQ:push_left(nb) end -- need to check -> can be just updating pred with a lower hight 
+                navGraph[nb.x][nb.y] = {pred= currLoc, predHig=graphGrid[currLoc.x][currLoc.y].loc.y, dis = navGraph[currLoc.x][currLoc.y].dis + 1}
             end
+
         end
+    end
+
+
+    -- find path in the navgraph trough going from start via .pred references
+    if navGraph[start.x] == nil or navGraph[start.x][start.y] == nil then
+        return {suc=false, dta=nil}
     end
 
     local currLoc = start
     local path = {graphGrid[currLoc.x][currLoc.y].loc}
     while currLoc.x ~= dest.x or currLoc.y ~= dest.y do
-        currLoc = navGraph[currLoc.x][currLoc.y]
+        currLoc = navGraph[currLoc.x][currLoc.y].pred
         path[#path+1] = graphGrid[currLoc.x][currLoc.y].loc
     end
 
-    return path
+    return {suc=true, dta=path}
 end
